915188f9533424419d6974961ec367c5daea33b0
erict <erict@MSI>
2023-03-17 18:51:50 +0800

    加入專案檔案。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b=0;
+    
+    cin >> a;
+    for (int i = 1; i <= a; i++) {
+        if (i % 3 == 0){
+            b += i;
+        }
+    }
+    cout<< b<<endl;
+        
+    return 0;
+}
